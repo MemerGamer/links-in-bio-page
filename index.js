779b84302cc69cfb80e729eab00afc695dda6116
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-06-27 12:10:33 +0300

    making animation smoother

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ window.addEventListener("DOMContentLoaded", () => {
     const main = document.querySelector("main");
     main.style.opacity = 1;
     main.style.filter = "blur(0px)";
-  }, 1000);
+  }, 100);
 });
